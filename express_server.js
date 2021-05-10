ddb2a74faa1e0e73c29134f5eba7522048187655
Matt <mattluo604@hotmail.com>
2021-05-10 21:38:44 +0000

    modified with ejs

--- express_server.js
+++ express_server.js
@@ -7,8 +7,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
 app.get("/urls.json", (req, res) => {
@@ -19,6 +36,14 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
