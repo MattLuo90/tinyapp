e59f3af5f683be4cc2dde9d779c6e0f5279a18d0
Matt <mattluo604@hotmail.com>
2021-05-11 00:30:09 +0000

    final version of day 1 tinyapp

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
 const bodyParser = require("body-parser");
 const generateRandomString = () => {
   let random = '';
@@ -38,10 +38,18 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
+app.post("/urls", (req, res) => {
+  const randomString = generateRandomString();
+  const newShortURL = `http://localhost:${PORT}/urls/${randomString}`;
+  urlDatabase[randomString] = req.body.longURL; 
+  res.redirect(newShortURL);        
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
