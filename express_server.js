97f33be349fef5a8586378eeb6b91ce0d79d1cdb
Matt <mattluo604@hotmail.com>
2021-05-13 00:39:57 +0000

    refactored login function

--- express_server.js
+++ express_server.js
@@ -4,7 +4,8 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 8080; 
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+
 
 //function to generate 6 new random charactors.
 const generateRandomString = () => {
@@ -16,7 +17,7 @@ const generateRandomString = () => {
   return random;
 };
 
-const emailCheck = (users, emailToBeChecked) => {
+const emailCheck = (emailToBeChecked) => {
   for (const key in users) {
     if (users[key].email === emailToBeChecked ) {
       return true;
@@ -24,6 +25,25 @@ const emailCheck = (users, emailToBeChecked) => {
   }
   return false;
 }
+const passwordCheck = (matchedEmail, passwordToBeChecked) => {
+  for (const key in users) {
+    if (users[key].email === matchedEmail) {
+      if (users[key].password === passwordToBeChecked) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+const idCheck = (matchedEmail) => {
+  for (const key in users) {
+    if (users[key].email === matchedEmail) {
+      return key;
+    }
+  }
+  return false;
+}
+
 
 //milldeware
 app.use(bodyParser.urlencoded({extended: true}));
@@ -55,18 +75,54 @@ app.get("/register", (req, res) => {
   res.render("registration");
 })
 
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+
+//the page operate a function that create a new short & long URL pair.
+app.get("/urls/new", (req, res) => { 
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  templateVars.user = users[req.cookies['newID']];
+  res.render("urls_index", templateVars)
+});
+
+//the page that shows the new short & long URL pair.
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+//perform a function that shows urlDatabase on urls page
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  templateVars.user = users[req.cookies['newID']];
+  res.render("urls_index", templateVars)
+});
+
+//the request that can redirect to a shortURL page
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.redirect(400, '/register');
   }
-  if (emailCheck(users, req.body.email)) {
-    return res.send("This email address has alreay been registered., please use another one.")
-  }
+  if (emailCheck(req.body.email)) {
+    res.send("This email address has alreay been registered., please use another one.");
+    return;
+}
   const randomUserID = generateRandomString();
   const newUser = {
     id: randomUserID,
     email: req.body.email,
-    passwprd: req.body.password
+    password: req.body.password
   };
   users[randomUserID] = newUser;
   res.cookie('newID', randomUserID);
@@ -74,6 +130,16 @@ app.post("/register", (req, res) => {
   res.redirect("/urls");
 })
 
+//function to login to web
+app.post("/login", (req, res) => {
+  console.log(req.body.password)
+  if(emailCheck(req.body.email) && passwordCheck(req.body.email, req.body.password)) {
+    res.cookie("newID", idCheck(req.body.email));
+    res.redirect("/urls");
+    return;
+  }
+  res.redirect(403, "/login");
+});
 
 //perform a function that can generate a new random string for a long URL.
 app.post("/urls", (req, res) => {                                 
@@ -83,15 +149,6 @@ app.post("/urls", (req, res) => {
   res.redirect(newShortURL);        
 });
 
-//perform a function that shows urlDatabase on urls page
-app.get("/urls", (req, res) => {
-  const templateVars = {
-    urls: urlDatabase,
-  };
-  templateVars.user = users[req.cookies['newID']];
-  console.log(templateVars)
-  res.render("urls_index", templateVars)
-});
 
 //perform a function that can delete a exist URL
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -106,10 +163,6 @@ app.post("/urls/:shortURL/update", (req, res) => {
   res.redirect("/urls")
 });
 
-//function to login to web
-app.post("/login", (req, res) => {
-  res.redirect("/urls")
-});
 
 //function to logout from web
 app.post("/logout", (req, res) => {
@@ -117,26 +170,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls")
 });
 
-//the page operate a function that create a new short & long URL pair.
-app.get("/urls/new", (req, res) => { 
-  const templateVars = {
-    urls: urlDatabase,
-  };
-  templateVars.users = users[req.cookie["newID"]];
-  res.render("urls_index", templateVars)
-});
-
-//the page that shows the new short & long URL pair.
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-})
-
-//the request that can redirect to a shortURL page
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
 
 //the function declare the port that the server runing on.
 app.listen(PORT, () => {
